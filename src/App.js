15d00b473c5d54c27a9ba4880063585770aebcd1
sem-aa <66129330+sem-aa@users.noreply.github.com>
2021-06-02 18:53:13 +0300

    change app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Hello world</h3>
         <p>
          Go start project Dashboard
         </p>
