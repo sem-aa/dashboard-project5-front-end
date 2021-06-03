0f2ba344181b214329a7eb9c234b24393b7d8e08
sem-aa <66129330+sem-aa@users.noreply.github.com>
2021-06-04 00:38:03 +0300

    add service-axios

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Container from './components/Container/Container'
 
 function App() {
   return (
-     <Container>
-    <div className="App">
+    <Container>
+      <div className="App">
       <header className="App-header">
       <p>Go start project Dashboard</p>    
         </header>
