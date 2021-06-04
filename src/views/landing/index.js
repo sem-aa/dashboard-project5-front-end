85bd14f6d0cb11757bd8ee30f90fa0f5d544c3ed
IrinaDiachenco <bilairina@ukr.net>
2021-06-04 21:16:27 +0300

    view + page landing

---
+++ src/views/landing/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../../components/Container/Container'
+import s from './landing.module.css'
+
+const landing = () => (
+
+        <Container>
+        <div className={s.landing}>
+        </div>
+      </Container>
+    
+  
+);
+
+export default landing;
