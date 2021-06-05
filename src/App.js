f28b1476f1b9a5decaec4886065ea6638b644695
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2021-06-05 12:06:58 +0300

    added route by authorization

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './App.css';
 import MainPage from './views/main';
 import landing from '../src/views/landing';
+import { getIsAuthenticated } from './redux/selectors/authSelectors';
 
+const App = () => {
+  const isAuthenticated = useSelector(getIsAuthenticated);
 
-const App = () => (
-  <>
-  <Switch>
-    <Route path="/" exact component={landing} />
-    <Route path="/main" component={MainPage} />
-    </Switch>
-  </>
-);
-
-
+  return (
+    <>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          component={isAuthenticated ? MainPage : landing}
+        />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
