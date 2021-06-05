8d8c66a516aba166956a219393630a23ec9a6f39
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2021-06-05 22:54:11 +0300

    add card, basic

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,29 @@
-import axios from "axios"
+import axios from 'axios';
 
-
-axios.defaults.baseURL = "https://questify-backend.goit.global"
+axios.defaults.baseURL = 'https://questify-backend.goit.global';
 
 const token = {
-    set(token) {
-        axios.defaults.headers.common.AutAuthorization = `Bearer ${token}`
-    },
-    unset() {
-      axios.defaults.headers.common.Authorization = ""
-    },
-}
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
 
 /* AUTHORIZATION */
-const signUp = credentials => axios.post('/auth/register', credentials)
-const logIn = credentials => axios.post('/auth/login', credentials)
-const logOut = () => axios.post('/auth/logout')
-const refreshToken = sid => axios.post('/auth/refresh', sid)
+const signUp = credentials => axios.post('/auth/register', credentials);
+const logIn = credentials => axios.post('/auth/login', credentials);
+const logOut = () => axios.post('/auth/logout');
+const refreshToken = sid => axios.post('/auth/refresh', sid);
 // const googleAuth = () => axios.get('/auth/google')
 
 /* Card */
-const createCard = data => axios.post('/card', data)
-const editCard = (id, data) => axios.patch(`/card/${id}`, data)
-const deleteCard = id => axios.delete(`/card/${id}`)
-const getAllCards = () => axios.get('/card')
-const completeCard = id => axios.patch(`/card/complete/${id}`)
+const createCard = data => axios.post('/card', data);
+const editCard = (id, data) => axios.patch(`/card/${id}`, data);
+const deleteCard = id => axios.delete(`/card/${id}`);
+const getAllCards = () => axios.get('/card');
+const completeCard = id => axios.patch(`/card/complete/${id}`);
 
 // eslint-disable-next-line
 export default {
@@ -33,11 +32,9 @@ export default {
   logIn,
   logOut,
   refreshToken,
-    createCard,
-    editCard,
-    deleteCard,
-    getAllCards,
-    completeCard
+  createCard,
+  editCard,
+  deleteCard,
+  getAllCards,
+  completeCard,
 };
-
-
