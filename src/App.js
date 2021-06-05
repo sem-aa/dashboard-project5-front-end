3bca711950fa208625aafd3b9ddaf8964c66ec86
sem-aa <66129330+sem-aa@users.noreply.github.com>
2021-06-05 09:09:26 +0300

    add service and redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import TestAuthForm from './services/TestAuthForm'
 
 function App() {
 
 
-  return <Header />;
-
+  return (
+    <>
+        <Header />
+        <TestAuthForm/>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
+
 import 'modern-normalize/modern-normalize.css';
 
 import './index.css';
@@ -8,9 +12,21 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+        </PersistGate>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 reportWebVitals();
+
+
+
+
+
+
+
+
