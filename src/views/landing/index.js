70eb2eb5e608620274ff845f0600b2bd39e81a3b
sem-aa <66129330+sem-aa@users.noreply.github.com>
2021-06-05 09:23:21 +0300

    add test form

--- src/views/landing/index.js
+++ src/views/landing/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Container from '../../components/Container/Container'
 import s from './landing.module.css'
+import Form from '../../services/TestAuthForm'
 
 const landing = () => (
 
-        <Container>
-        <div className={s.landing}>
+  <Container>
+    <Form/>
+    <div className={s.landing}>
+    
         </div>
       </Container>
     
