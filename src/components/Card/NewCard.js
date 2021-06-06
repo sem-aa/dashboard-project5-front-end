2200a3e41a12ea4e84eff1c6d65c3eaa4cd0d6ac
sem-aa <66129330+sem-aa@users.noreply.github.com>
2021-06-06 19:30:08 +0300

    fix button

--- src/components/Card/NewCard.js
+++ src/components/Card/NewCard.js
@@ -25,7 +25,7 @@ const Card = React.forwardRef(({ data, register, handleSubmit, isEdit }, ref) =>
                     {isEdit ?
                         <div className={s.dateFlex}>
                         <input className={s.inputDate} {...register('date')} ref={ref} placeholder="Today"></input>
-                        <button ref={ref}>
+                        <button className={s.buttonCard} ref={ref}>
                                 <svg className={ s.calendar}height="10px" width="10px" >
                     <use href={sprite + "#icon-calendar"}></use>
                     </svg>
@@ -46,17 +46,17 @@ const Card = React.forwardRef(({ data, register, handleSubmit, isEdit }, ref) =>
                     <div>
                 {isEdit && 
                 <div className={s.buttonFlex}>
-                <button ref={ref}>
+                <button className={s.buttonCard} ref={ref}>
                 <svg className={s.buttonEdit} >
                     <use href={sprite + "#icon-done"}></use>
                     </svg>
                 </button>
-                <button ref={ref}>
+                <button className={s.buttonCard} ref={ref}>
                 <svg className={s.buttonEdit}>
                     <use href={sprite + "#icon-save"}></use>
                     </svg>
                 </button >
-                <button ref={ref}>
+                <button className={s.buttonCard}  ref={ref}>
                 <svg className={s.buttonEdit}>
                     <use href={sprite + "#icon-clear"}></use>
                 </svg>
