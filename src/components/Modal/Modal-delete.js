d5ad98c2ce3be42caaa42a2a3061c431f9a77ca3
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2021-06-06 12:25:32 +0300

    test

---
+++ src/components/Modal/Modal-delete.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Modal-delete.module.css';
+//import PropTypes from 'prop-types';//
+
+const ModalDelete = ({}) => (
+    <div className={s.Overlay}>
+        <p className={s.Name}>Delete this Quest</p>
+        <li className={s.Cancel}>CANCEL</li>
+        <li className={s.Delete}>DELETE</li>
+    </div>
+);
+
+//ModalDelete.propTypes = {};//
+
+export default ModalDelete;
--- src/components/Modal/Modal-hard.js
+++ src/components/Modal/Modal-hard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstreamтзь
 import s from "./Modal-hard.module.css";
 import sprite from '../../icon/sprite.svg';
 import s from './svg.module.css';
@@ -23,5 +24,19 @@ const ModalHard = () => {
         </div>
   )
 };
+=======
+import s from './Modal-hard.module.css';
+//import PropTypes from 'prop-types';//
+
+const ModalHard = ({  }) => (
+    <div className={s.Overlay}>
+        <li className={s.Li}>Easy</li>
+        <li className={s.Li}>Normal</li>
+        <li className={s.Li}>Hard</li>
+    </div>
+);
+
+//ModalDelete.propTypes = {};//
+>>>>>>> Stashed changes
 
 export default ModalHard;
