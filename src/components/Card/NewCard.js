f8e6f7c7d31befa0b03355e8e9dd216b93dddc0e
sem-aa <66129330+sem-aa@users.noreply.github.com>
2021-06-07 10:44:00 +0300

    fix card

--- src/components/Card/NewCard.js
+++ src/components/Card/NewCard.js
@@ -6,143 +6,142 @@ import { useDispatch } from 'react-redux';
 import { createCard } from '../../redux/operations/cardOperations';
 
 const Card = React.forwardRef(({ data, register, handleSubmit, isEdit }, ref) => {
-  return (
-    <div className={s.container}>
-      <form onSubmit={handleSubmit}>
-        <div className={s.head}>
-          <select
-            className={s.difficulty}
-            name={'difficulty'}
-            defaultValue={data.difficulty}
-            ref={ref}
-            {...register('difficulty')}
-          >
-            <option
-              className={s.item}
-              value="Easy"
-              selected={'Easy' === data.difficulty && 'selected'}
-            >
-              Easy
+    return (
+        <div className={s.container}> <form onSubmit={handleSubmit}>
+            <div className={s.head}>
+                <select
+                    className={s.difficulty}
+                    name={'difficulty'}
+                    defaultValue={data.difficulty}
+                    ref={ref}
+                    {...register('difficulty')}
+                >
+                    <option
+                        className={s.item}
+                        value="Easy"
+                        selected={'Easy' === data.difficulty && 'selected'}
+                    >
+                        Easy
+    </option>
+                    <option value="Normal" selected={'Normal' === data.difficulty && 'selected'}>
+                        Normal
             </option>
-            <option value="Normal" selected={'Normal' === data.difficulty && 'selected'}>
-              Normal
+                    <option value="Hard" selected={'Hard' === data.difficulty && 'selected'}>
+                        Hard
             </option>
-            <option value="Hard" selected={'Hard' === data.difficulty && 'selected'}>
-              Hard
-            </option>
-          </select>
-          <svg className={s.iconTask}>
-            <use href={sprite + '#icon-star'}></use>
-          </svg>
-          {/* <select className={s.task} name={'type'} ref={ref} {...register('type')}>
+                </select>
+                <svg className={s.iconTask}>
+                    <use href={sprite + '#icon-star'}></use>
+                </svg>
+                {/* <select className={s.task} name={'type'} ref={ref} {...register('type')}>
                     <option value="Task">Task</option>
             <option  value="Challenge">Challenge</option>
                     </select> */}
-        </div>
-
-        <div>
-          {isEdit ? (
-            <input
-              className={s.titleInput}
-              {...register('title')}
-              defaultValue={data.title}
-              ref={ref}
-            ></input>
-          ) : (
-            <h2 className={s.title}>{data.title}</h2>
-          )}
-          {isEdit ? (
-            <div className={s.dateFlex}>
-              <input
-                className={s.inputDate}
-                {...register('date')}
-                ref={ref}
-                placeholder="Today"
-              ></input>
-              <button ref={ref}>
-                <svg className={s.calendar} height="10px" width="10px">
-                  <use href={sprite + '#icon-calendar'}></use>
-                </svg>
-              </button>
             </div>
-          ) : (
-            <p className={s.date}>March 23, 21:30</p>
-          )}
+
+            <div>
+                {isEdit ? (
+                    <input
+                        className={s.titleInput}
+                        {...register('title')}
+                        defaultValue={data.title}
+                        ref={ref}
+                    ></input>
+                ) : (
+                    <h2 className={s.title}>{data.title}</h2>
+                )}
+                {isEdit ? (
+                    <div className={s.dateFlex}>
+                        <input
+                            className={s.inputDate}
+                            {...register('date')}
+                            ref={ref}
+                            placeholder="Today"
+                        ></input>
+                        <button ref={ref}>
+                            <svg className={s.calendar} height="10px" width="10px">
+                                <use href={sprite + '#icon-calendar'}></use>
+                            </svg>
+                        </button>
+                    </div>
+                ) : (
+                    <p className={s.date}>March 23, 21:30</p>
+                )}
+            </div>
+            <div className={s.foot}>
+                <select className={s.category} name={'category'} ref={ref} {...register('category')}>
+                    <option className={s.stuff} value="Stuff" ref={ref}>
+                        Stuff
+            </option>
+                    <option value="Family" selected={data.category === 'Family' && 'selected'}>
+                        FAMILY
+            </option>
+                    <option value="Health" selected={data.category === 'Health' && 'selected'}>
+                        HEALTH
+            </option>
+                    <option value="Learning" selected={data.category === 'Learning' && 'selected'}>
+                        LEARNING
+            </option>
+                    <option value="Leisure" selected={data.category === 'Leisure' && 'selected'}>
+                        LEISURE
+            </option>
+                    <option value="Stuff" selected={data.category === 'Stuff' && 'selected'}>
+                        STUFF
+            </option>
+                    <option value="Work" selected={data.category === 'Work' && 'selected'}>
+                        WORK
+            </option>
+                </select>
+                <div>
+                    {isEdit && (
+                        <div className={s.buttonFlex}>
+                            <button className={s.buttonCard} ref={ref} type="submit">
+                                <svg className={s.buttonSave}>
+                                    <use href={sprite + '#icon-save'}></use>
+                                </svg>
+                            </button>
+                            <button className={s.buttonClose} ref={ref}>
+                                <svg className={s.buttonClear}>
+                                    <use href={sprite + '#icon-clear'}></use>
+                                </svg>
+                            </button>
+                            <button className={s.buttonCard} ref={ref}>
+                                <svg className={s.buttonDone}>
+                                    <use href={sprite + '#icon-done'}></use>
+                                </svg>
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </form>
         </div>
-        <div className={s.foot}>
-          <select className={s.category} name={'category'} ref={ref} {...register('category')}>
-            <option className={s.stuff} value="Stuff" ref={ref}>
-              Stuff
-            </option>
-            <option value="Family" selected={data.category === 'Family' && 'selected'}>
-              FAMILY
-            </option>
-            <option value="Health" selected={data.category === 'Health' && 'selected'}>
-              HEALTH
-            </option>
-            <option value="Learning" selected={data.category === 'Learning' && 'selected'}>
-              LEARNING
-            </option>
-            <option value="Leisure" selected={data.category === 'Leisure' && 'selected'}>
-              LEISURE
-            </option>
-            <option value="Stuff" selected={data.category === 'Stuff' && 'selected'}>
-              STUFF
-            </option>
-            <option value="Work" selected={data.category === 'Work' && 'selected'}>
-              WORK
-            </option>
-          </select>
-          <div>
-            {isEdit && (
-              <div className={s.buttonFlex}>
-                <button className={s.buttonCard} ref={ref} type="submit">
-                  <svg className={s.buttonSave}>
-                    <use href={sprite + '#icon-save'}></use>
-                  </svg>
-                </button>
-                <button className={s.buttonClose} ref={ref}>
-                  <svg className={s.buttonClear}>
-                    <use href={sprite + '#icon-clear'}></use>
-                  </svg>
-                </button>
-                <button className={s.buttonCard} ref={ref}>
-                  <svg className={s.buttonDone}>
-                    <use href={sprite + '#icon-done'}></use>
-                  </svg>
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+    );
 });
 
 export default function CardForm({ data }) {
-  const [isEdit, setEdit] = useState(true);
-  const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm();
+    const [isEdit, setEdit] = useState(false);
+    const dispatch = useDispatch();
+    const { register, handleSubmit } = useForm();
 
-  const onSubmit = data => {
-    const body = {
-      ...data,
-      type: 'Task',
-      date: new Date().toISOString().split('T')[0],
-      time: new Date().getHours() + ':' + new Date().getMinutes(),
+    const onSubmit = data => {
+        const body = {
+            ...data,
+            type: 'Task',
+            date: new Date().toISOString().split('T')[0],
+            time: new Date().getHours() + ':' + new Date().getMinutes(),
+        };
+
+        dispatch(createCard(body));
     };
 
-    dispatch(createCard(body));
-  };
-
-  return (
-    <Card
-      onClick={() => setEdit(!isEdit)}
-      isEdit={isEdit}
-      handleSubmit={handleSubmit(onSubmit)}
-      register={register}
-      data={data}
-    />
-  );
+    return (
+        <Card
+            onClick={() => setEdit(!isEdit)}
+            isEdit={isEdit}
+            handleSubmit={handleSubmit(onSubmit)}
+            register={register}
+            data={data}
+        />
+    );
 }
--- src/views/TestSvg.js
+++ src/views/TestSvg.js
@@ -7,82 +7,75 @@ import Select from '../components/Card/Select'
 
 
 export default function TestSvg() {
-    return (
-      <>
-        <Select/>
-        
-        <Card/>
+  return (
+    <>
+      <Select />
+      <Card />
       <svg>
-        <use  className={s.icon} width="14px" height="14px"
-        href={sprite + "#icon-done"}></use>
-        </svg>
+        <use className={s.icon} width="14px" height="14px"
+          href={sprite + "#icon-done"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-save"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-save"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-arrow"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-arrow"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-go"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-go"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-fire"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-fire"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-plus"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-plus"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-star"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-star"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-clear"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-clear"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-calendar"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-calendar"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-trophy"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-trophy"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-polygon"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-polygon"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-ellipse"}></use>
-        </svg>
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-ellipse"}></use>
+      </svg>
 
-        <svg>
-        <use className={s.icon} 
-        href={sprite + "#icon-logout"}></use>
-        </svg>
-
-        
-        
-            
-            
-        
+      <svg>
+        <use className={s.icon}
+          href={sprite + "#icon-logout"}></use>
+      </svg>
     </>
-    );
-    
+  );
+
 }
