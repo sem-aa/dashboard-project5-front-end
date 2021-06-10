2abd81afdf56df2ddb1a75a01fc7e88e24a8d478
Volodymyr Ivanov <3298901@gmail.com>
2021-06-10 15:32:15 +0300

    current changes in completeForm

--- src/components/Card/CompleteForm/CompleteForm.jsx
+++ src/components/Card/CompleteForm/CompleteForm.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 import sprite from '../../../icon/sprite.svg';
 import s from './CompleteForm.module.css'
 
-export default function CompleteForm () {
+export default function CompleteForm({ title }) {
   return (
     <section className={s.completed}>
-          <div className={s.completedHeader}>
-            <h2 className={s.statusCompleted}>completed:</h2>
-            {/* <p>{data.title}</p> */}
-            <p>Title Card</p>
-          </div>
-          <svg className={s.award}>
-            <use href={sprite + '#icon-award'}></use>
+      <div className={s.completedHeader}>
+        <h2 className={s.statusCompleted}>completed:</h2>
+        <p className={s.completedTitle}>{title}</p>
+      </div>
+      <svg className={s.award}>
+        <use href={sprite + '#icon-award'}></use>
+      </svg>
+      <div className={s.completedFooter}>
+        <p>Continue</p>
+        <button
+          className={s.buttonCard}
+          onClick={() => {
+            console.log('goto page "Done" ');
+          }}
+        >
+          <svg className={s.arrow}>
+            <use href={sprite + '#icon-arrow'}></use>
           </svg>
-          <div className={s.completedFooter}>
-            <p>Continue</p>
-            <button
-              className={s.buttonCard}
-              onClick={() => {
-                console.log('goto page "Done" ');
-              }}
-            >
-              <svg className={s.arrow}>
-                <use href={sprite + '#icon-arrow'}></use>
-              </svg>
-            </button>
-          </div>
-        </section>
+        </button>
+      </div>
+    </section>
   )
 }
