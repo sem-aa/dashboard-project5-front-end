3967629d885535e3ea60476ee873422767ecd83f
IrinaDiachenco <bilairina@ukr.net>
2021-06-11 11:24:56 +0300

    lazy route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './App.css';
-import MainPage from './views/main';
-import landing from '../src/views/landing';
-import Svg from './views/TestSvg';
+import Loader from 'react-loader-spinner';
+// import MainPage from './views/main';
+// import landing from './views/landing';
 import { getIsAuthenticated } from './redux/selectors';
+
+const LandingPage = lazy(() => import('./views/landing' /*webpackChunkName: "landingPage" */));
+const MainPage = lazy(() => import('./views/main' /*webpackChunkName: "mainPage" */));
 
 const App = () => {
   const isAuthenticated = useSelector(getIsAuthenticated);
   return (
     <>
-      <Switch>
-        <Route path="/" exact component={isAuthenticated ? MainPage : landing} />
-        <Route path="/landing" component={landing} />
-        <Route path="/svg" component={Svg} />
-      </Switch>
+      <Suspense fallback={<Loader color='var(--bg-header)'/>}>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/main" component={isAuthenticated ? MainPage : LandingPage} />
+        </Switch>
+      </Suspense>
     </>
   );
 };
