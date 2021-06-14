035ea661200749f772a7b1979cca33cb1c18e403
Volodymyr Ivanov <3298901@gmail.com>
2021-06-14 03:49:47 +0300

    added alerts

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,87 +1,105 @@
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getError } from '../../redux/selectors'
-import authOperations from '../../redux/operations/authOperetions'
-import ButtonGo from '../Buttons/ButtonGo/ButtonGo'
-import ButtonSign from '../Buttons/ButtonGo/ButtonSign'
-import s from './AuthForm.module.css'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getError } from '../../redux/selectors';
+import authOperations from '../../redux/operations/authOperetions';
+import ButtonGo from '../Buttons/ButtonGo/ButtonGo';
+import ButtonSign from '../Buttons/ButtonGo/ButtonSign';
+import s from './AuthForm.module.css';
+
+// =================ALRT========================
+import Alert from './Alert';
+import { CSSTransition } from 'react-transition-group';
+import st from './Alert/Alert.module.css';
+// ---------------------------------------------
 
 const AuthForm = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-  const error = useSelector(getError)
-  const dispatch = useDispatch()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-  const changeEmailValue = event => setEmail(event.target.value)
-  const changePasswordValue = event => setPassword(event.target.value)
+  // =================ALRT========================
+  const [isError, setIsError] = useState(false);
+  const getAlert = () => {
+    setIsError(true);
+    setTimeout(() => setIsError(false), 3000);
+  };
+  const showAlert = (setstate, message) => {
+    setstate(message);
+    getAlert();
+  };
+
+  // ---------------------------------------------
+  const error = useSelector(getError);
+  const dispatch = useDispatch();
+
+  const changeEmailValue = event => setEmail(event.target.value);
+  const changePasswordValue = event => setPassword(event.target.value);
 
   const onSubmit = event => {
-    event.preventDefault()
+    event.preventDefault();
 
     !validateEmail(email)
-      ? setEmailError('Некорректно введен e-mail.')
-      : setEmailError('')
+      ? showAlert(setEmailError, 'Некорректно введен e-mail.')
+      : setEmailError('');
 
     !validatePassword(password)
-      ? setPasswordError('Пароль должен быть от 4 до 16 символов.')
-      : setPasswordError('')
+      ? showAlert(setPasswordError, 'Пароль должен быть от 4 до 16 символов.')
+      : setPasswordError('');
 
-    !email && setEmailError('это обязательное поле')
-    !password && setPasswordError('это обязательное поле')
+    !email && showAlert(setEmailError, 'email - обязательное поле');
+    !password && showAlert(setPasswordError, 'пароль - обязательное поле');
 
     if (validateEmail(email) && validatePassword(password)) {
       dispatch(authOperations.handleLogIn({ email, password }));
-      formReset()
+      formReset();
     }
   };
 
   const onRegistration = () => {
     !validateEmail(email)
-      ? setEmailError('Некорректно введен e-mail.')
-      : setEmailError('')
+      ? showAlert(setEmailError, 'Некорректно введен e-mail.')
+      : setEmailError('');
 
     !validatePassword(password)
-      ? setPasswordError('Пароль должен быть от 4 до 16 символов.')
-      : setPasswordError('')
-    !email && setEmailError('это обязательное поле')
-    !password && setPasswordError('это обязательное поле')
+      ? showAlert(setPasswordError, 'Пароль должен быть от 4 до 16 символов.')
+      : setPasswordError('');
+    !email && showAlert(setEmailError, 'email - обязательное поле');
+    !password && showAlert(setPasswordError, 'пароль - обязательное поле');
 
     if (validateEmail(email) && validatePassword(password)) {
-      dispatch(authOperations.handleSignUp({ email, password }))
+      dispatch(authOperations.handleSignUp({ email, password }));
       formReset();
     }
-  }
+  };
 
   const validateEmail = email => {
     // eslint-disable-next-line
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return re.test(email)
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
   };
 
   const validatePassword = password => {
-    return Boolean(password.length >= 4 && password.length <= 16)
-  }
+    return Boolean(password.length >= 4 && password.length <= 16);
+  };
 
   const formReset = () => {
-    setEmail('')
-    setPassword('')
+    setEmail('');
+    setPassword('');
   };
 
   const errorMessage = () => {
     if (error === 'Request failed with status code 409') {
-      return 'Пользователь с таким email уже зарегистрирован'
+      return 'Пользователь с таким email уже зарегистрирован';
     } else if (error === 'Request failed with status code 403') {
       return 'Некорректный пароль или email';
     }
-  }
-
+  };
 
   return (
     <>
       <form className={s.landingForm} onSubmit={onSubmit}>
-
         <div className={s.landingBox}>
           <label htmlFor="AuthorizationForm__email">
             {emailError && <span style={{ color: 'red' }}>*</span>}
@@ -95,11 +113,17 @@ const AuthForm = () => {
             onChange={changeEmailValue}
             placeholder="Email"
           />
-          <p style={{ color: 'black' }} >{emailError}</p>
+          {/* <p style={{ color: 'black' }}>{emailError}</p> */}
+          {/* =================ALRT======================== */}
+          {emailError && (
+            <CSSTransition in={isError} classNames={st} unmountOnExit timeout={250}>
+              <p className={st.alert}> {emailError}</p>
+            </CSSTransition>
+          )}
+          {/* --------------------------------------------- */}
         </div>
-        <div className={s.landingBox} >
-          <label
-            htmlFor="AuthorizationForm__password">
+        <div className={s.landingBox}>
+          <label htmlFor="AuthorizationForm__password">
             {passwordError && <span style={{ color: 'red' }}>*</span>}
             Пароль:
           </label>
@@ -111,21 +135,27 @@ const AuthForm = () => {
             onChange={changePasswordValue}
             placeholder="Пароль"
           />
-          <p style={{ color: 'black' }}>{passwordError}</p>
+          {/* <p style={{ color: 'black' }}>{passwordError}</p> */}
+          {/* =================ALRT======================== */}
+          {passwordError && (
+            <CSSTransition in={isError} classNames={st} unmountOnExit timeout={250}>
+              <p className={st.alert}> {passwordError}</p>
+            </CSSTransition>
+          )}
+          {/* --------------------------------------------- */}
         </div>
-        <div className={s.btnGo} >
-          <ButtonGo
-            type="submit"
-          />
-          <ButtonSign
-            type="button"
-            onClick={onRegistration}
-          />
+        <div className={s.btnGo}>
+          <ButtonGo type="submit" />
+          <ButtonSign type="button" onClick={onRegistration} />
         </div>
-        <p style={{ color: 'black' }} >{errorMessage()}</p>
+        {error && (
+          <CSSTransition in={isError} classNames={st} unmountOnExit timeout={250}>
+            <p style={{ color: 'black' }}>{errorMessage()}</p>
+          </CSSTransition>
+        )}
       </form>
     </>
   );
 };
 
-export default AuthForm
+export default AuthForm;
