00f1b9330591ebef5ba26df7604429d8e20b53af
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2021-06-20 17:28:40 +0300

    resolved conflicts

--- src/components/LandingText/LandingText.js
+++ src/components/LandingText/LandingText.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import style from './LandingText.module.css';
-import Form from '../AuthForm/AuthForm'
-
+import Form from '../AuthForm/AuthForm';
 
 const LandingText = () => {
-  const [register, setRegister] = useState(false)
+  const [registered, setRegistered] = useState(true);
 
   return (
     <>
@@ -12,16 +11,26 @@ const LandingText = () => {
         <h1 className={style.landingTittle}>Questify</h1>
       </div>
       <div className={style.landingDescriptionBox}>
-        <p className={style.landingDescription}>Questify will turn your life into
-          a thrilling game full of amazing
-          quests and exciting challenges.</p>
+        <p className={style.landingDescription}>
+          Questify will turn your life into a thrilling game full of amazing quests and exciting
+          challenges.
+        </p>
       </div>
       <div className={style.landingRequestBox}>
-        <p className={style.landingRequest}>Write your email to <span className={style.linkAuth} onClick={() => setRegister(true)} >sign up</span>  or <span className={style.linkAuth} onClick={() => setRegister(false)}>log in</span></p>
+        <p className={style.landingRequest}>
+          Write your email to
+          <span className={style.linkAuth} onClick={() => setRegistered(false)}>
+            sign up
+          </span>
+          or
+          <span className={style.linkAuth} onClick={() => setRegistered(true)}>
+            log in
+          </span>
+        </p>
       </div>
-      <Form register={register} />
-    </>)
-
-}
+      <Form registered={registered} />
+    </>
+  );
+};
 
 export default LandingText;
--- src/redux/operations/handleError.js
+++ src/redux/operations/handleError.js
@@ -1,22 +1,14 @@
 import operations from './authOperations';
 
 export default async function handleError(err, dispatch, fn, data = {}) {
-  console.dir(err);
-  console.log(err?.response?.data?.message === 'Invalid session');
-  console.log(err?.response?.data?.message);
-  if (err?.massage === 'Invalid session' || err?.response?.data?.message === 'Invalid session') {
-    console.log('wprt');
-    return err;
-  }
-  console.log('не сработал ');
   try {
+    if (err?.massage === 'Invalid session' || err?.response?.data?.message === 'Invalid session') {
+      return err;
+    }
+
     if (err.response?.status === 401) {
       const error = await dispatch(operations.refreshToken());
       !error && fn && dispatch(fn(...Object.values(data)));
-    }
-
-    if (err.response?.status === 403) {
-      fn && dispatch(fn(data));
     }
   } catch (error) {
     return err;
